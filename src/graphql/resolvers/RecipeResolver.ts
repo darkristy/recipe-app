d3b4c87a98a6ec40dc4438e2b2230a2b17ac736c
Kahlil Whitfield <darkristy2002@gmail.com>
2021-01-26 23:19:19 -0500

    feat: working on recipe resolver

--- src/graphql/resolvers/RecipeResolver.ts
+++ src/graphql/resolvers/RecipeResolver.ts
@@ -1,15 +1,47 @@
-import { Query, Resolver } from "type-graphql";
-import { PrismaClient } from "@prisma/client";
+/* eslint-disable no-shadow */
+import { Authorized, Ctx, Query, Resolver } from "type-graphql";
+import { category, PrismaClient, recipe } from "@prisma/client";
 
-import { Recipe } from "../models";
+import { Category, Recipe, UserRole } from "../models";
 
 const prisma = new PrismaClient();
 
 @Resolver(Recipe)
 export class RecipeResolver {
-	@Query((returns) => [Recipe])
-	async recipe() {
-		const recipe = await prisma.recipe.findMany();
-		return recipe;
+	@Query(() => [Recipe])
+	@Authorized(UserRole.ADMIN)
+	async recipes(): Promise<recipe[]> {
+		const recipes = await prisma.recipe.findMany();
+		return recipes;
+	}
+
+	@Query(() => [Recipe])
+	@Authorized()
+	async userRecipes(@Ctx() ctx): Promise<recipe[]> {
+		const user = ctx.payload;
+		const currentUser = await prisma.user.findUnique({ where: { id: user.userId }, include: { recipes: true } });
+
+		return currentUser.recipes;
+	}
+
+	@Query(() => [Category])
+	@Authorized()
+	async userRecipeCategories(@Ctx() ctx): Promise<category[]> {
+		const categories = [];
+		const user = ctx.payload;
+		const currentUser = await prisma.user.findUnique({ where: { id: user.userId }, include: { recipes: true } });
+
+		const recipes = currentUser.recipes;
+
+		for (const recipe of recipes) {
+			const category = await prisma.category.findUnique({
+				where: {
+					id: recipe.categoryId,
+				},
+			});
+			categories.push(category);
+		}
+
+		return [...new Set(categories)];
 	}
 }
