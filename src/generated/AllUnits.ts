b3829c134e5ab89f54b076be1bbc9af56f1b9a6d
Kahlil Whitfield <darkristy2002@gmail.com>
2021-02-08 21:05:50 -0500

    chore: new types

---
+++ src/generated/AllUnits.ts
@@ -0,0 +1,17 @@
+/* tslint:disable */
+/* eslint-disable */
+// @generated
+// This file was automatically generated and should not be edited.
+
+// ====================================================
+// GraphQL query operation: AllUnits
+// ====================================================
+
+export interface AllUnits_measurmentUnits {
+  __typename: "MeasurmentUnit";
+  name: string;
+}
+
+export interface AllUnits {
+  measurmentUnits: AllUnits_measurmentUnits[];
+}
