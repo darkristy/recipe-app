e8c62d300ac26814b111c1d386ce73137b7c30c6
Kahlil Whitfield <darkristy2002@gmail.com>
2021-02-11 03:56:44 -0500

    chore: create recipe page getting close to done

--- src/pages/new-recipe.tsx
+++ src/pages/new-recipe.tsx
@@ -1,16 +1,19 @@
 import styled from "@emotion/styled";
-import { useFormik } from "formik";
+import { Field, FieldArray, FormikProvider, useFormik } from "formik";
 import { motion } from "framer-motion";
 import { NextPage } from "next";
 import { useRouter } from "next/router";
-import React from "react";
 import * as yup from "yup";
+import { useMutation } from "@apollo/client";
+import React from "react";
 
+import { CreateNewRecipe, CreateNewRecipeVariables } from "../generated/CreateNewRecipe";
+import Alert from "../components/Alert";
+import { CreateNewRecipeMutation } from "../graphql/queries/recipeMutations";
 import { Mixins } from "../styles/mixins";
 import { Wrapper } from "../styles/globals";
 import { useFetchCuisines, useFetchIngredients, useFetchUnits } from "../hooks/useOptions";
 import Form, { FormInput, FormSelect } from "../components/Form";
-import { withAuth } from "../lib/withAuth";
 import { withApollo } from "../lib/withApollo";
 import Button from "../components/Button";
 
@@ -22,33 +25,52 @@ const NewRecipeScreenStyles = {
 	Container: styled(motion.div)``,
 };
 
+interface FormIngredient {
+	qty: string;
+	unit: any;
+	ingredient: any;
+}
+
 interface FormInputs {
-	username: string;
-	password: string;
+	name: string;
+	prepTime: string;
+	cookTime: string;
+	cuisine: any;
+	imageUrl: string;
+	ingredients: [FormIngredient];
 }
 
 const NewRecipeScreen: NextPage<NewRecipeProps> = () => {
 	const router = useRouter();
 
+	const [submitRecipe, { error }] = useMutation<CreateNewRecipe, CreateNewRecipeVariables>(CreateNewRecipeMutation);
+
 	const units = useFetchUnits();
 	const cuisines = useFetchCuisines();
 	const ingredients = useFetchIngredients();
 
-	const { handleBlur, handleSubmit, handleChange, errors, values, touched, setFieldValue } = useFormik({
+	const formik = useFormik({
 		initialValues: {
 			name: "",
 			prepTime: "",
 			cookTime: "",
 			cuisine: {},
 			imageUrl: "",
-			recipeIngredient: {
-				qty: "",
-				unit: {},
-				ingredient: {},
-			},
+			ingredients: [
+				{
+					qty: "",
+					unit: {},
+					ingredient: {},
+				},
+			],
+			instructions: [
+				{
+					description: "",
+				},
+			],
 		},
 		onSubmit: async (input): Promise<void> => {
-			console.log(input.cuisine);
+			console.log(input);
 		},
 		validationSchema: yup.object().shape({
 			username: yup.string(),
@@ -58,78 +80,99 @@ const NewRecipeScreen: NextPage<NewRecipeProps> = () => {
 
 	let errorMessage;
 
-	// if (error) {
-	// 	errorMessage = error?.graphQLErrors.map((err) => err.message);
-	// }
+	if (error) {
+		errorMessage = error?.graphQLErrors.map((err) => err.message);
+	}
 	return (
 		<NewRecipeScreenStyles.Container exit={{ opacity: 0 }}>
 			<Wrapper>
-				<Form handleSubmit={handleSubmit}>
-					<FormInput
-						type="text"
-						name="Name"
-						onChange={handleChange}
-						touched={touched.name}
-						value={values.name}
-						error={errors.recipeIngredient?.qty}
-						onBlur={handleBlur}
-					/>
-					<FormSelect
-						id="Cuisine"
-						defaultOptions={cuisines.data?.cuisines}
-						value={values.cuisine}
-						setFieldValue={setFieldValue}
-						handleBlur={handleBlur}
-					/>
-					<Mixins.Flex>
-						<FormInput
-							type="text"
-							name="Prep Time"
-							onChange={handleChange}
-							touched={touched.prepTime}
-							value={values.prepTime}
-							error={errors.prepTime}
-							onBlur={handleBlur}
-							noMargin
+				<FormikProvider value={formik}>
+					<Form handleSubmit={formik.handleSubmit}>
+						<Field component={FormInput} name="name" />
+						<Mixins.Flex spaceBetween>
+							<Field component={FormInput} name="prepTime" noMargin />
+							<Field component={FormInput} name="cookTime" />
+						</Mixins.Flex>
+						<Field
+							component={FormSelect}
+							name="cuisine"
+							defaultOptions={cuisines.data?.cuisines}
+							setFieldValue={formik.setFieldValue}
 						/>
+						<Field component={FormInput} name="imageUrl" />
 
-						<FormInput
-							type="text"
-							name="Cook Time"
-							onChange={handleChange}
-							touched={touched.cookTime}
-							value={values.cookTime}
-							error={errors.cookTime}
-							onBlur={handleBlur}
-						/>
-					</Mixins.Flex>
-					<FormInput
-						type="text"
-						name="qty"
-						onChange={handleChange}
-						touched={touched.recipeIngredient?.qty}
-						value={values.recipeIngredient?.qty}
-						error={errors.recipeIngredient?.qty}
-						onBlur={handleBlur}
-					/>
-					<FormSelect
-						id="unit"
-						defaultOptions={units.data?.measurmentUnits}
-						value={values.recipeIngredient.unit}
-						setFieldValue={setFieldValue}
-						handleBlur={handleBlur}
-					/>
-					<FormSelect
-						id="ingredient"
-						defaultOptions={ingredients.data?.ingredients}
-						value={values.recipeIngredient.ingredient}
-						setFieldValue={setFieldValue}
-						handleBlur={handleBlur}
-						creatable
-					/>
+						<FieldArray name="ingredients">
+							{({ push }) => (
+								<>
+									{formik.values.ingredients.map((value, idx) => (
+										<React.Fragment key={idx}>
+											<Mixins.Flex spaceBetween>
+												<Field
+													name={`ingredients[${idx}].ingredient`}
+													component={FormSelect}
+													defaultOptions={ingredients.data?.ingredients}
+													setFieldValue={formik.setFieldValue}
+												/>
+												<Field name={`ingredients[${idx}].qty`} component={FormInput} />
+												<Field
+													name={`ingredients[${idx}].unit`}
+													component={FormSelect}
+													defaultOptions={units.data?.measurmentUnits}
+													setFieldValue={formik.setFieldValue}
+												/>
+											</Mixins.Flex>
+										</React.Fragment>
+									))}
+									<Button
+										type="button"
+										label="Add Ingredient"
+										size="small"
+										onClick={() =>
+											push({
+												qty: "",
+												unit: {},
+												ingredient: {},
+											})
+										}
+									/>
+								</>
+							)}
+						</FieldArray>
 
-					<Button type="submit" label="Create Recipe" size="full" primary />
-				</Form>
+						<FieldArray name="instructions">
+							{({ push }) => (
+								<>
+									{formik.values.instructions.map((value, idx) => (
+										<React.Fragment key={idx}>
+											<Mixins.Flex>
+												<Field
+													name={`instructions[${idx}].description`}
+													noMargin
+													component={FormInput}
+												/>
+											</Mixins.Flex>
+										</React.Fragment>
+									))}
+									<Button
+										type="button"
+										label="Add Step"
+										size="small"
+										onClick={() =>
+											push({
+												description: "",
+											})
+										}
+									/>
+								</>
+							)}
+						</FieldArray>
+
+						<Button type="submit" label="Create Recipe" size="full" primary />
+					</Form>
+				</FormikProvider>
+				<Mixins.Flex center style={{ paddingTop: 26 }}>
+					{error && <Alert message={errorMessage} severity="error" />}
+				</Mixins.Flex>
 			</Wrapper>
 		</NewRecipeScreenStyles.Container>
 	);
